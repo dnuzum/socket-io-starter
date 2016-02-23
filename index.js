482e825c36e96e0a13ae21cc7f382a045d0a6305
bhague1281 <bhague1281@gmail.com>
2016-02-22 19:41:30 -0800

    Finished socket

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+var http  = require('http').Server(app);
+var io = require('socket.io')(http);
+var Twit = require('twit');
 var port = process.env.PORT || 3000;
+
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+io.on('connect', function(socket) {
+  var stream = twitter.stream('statuses/filter', { track: 'javascript' });
+
+  stream.on('tweet', function(tweet) {
+    var data = {};
+    data.name = tweet.user.name;
+    data.screen_name = tweet.user.screen_name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit('tweets', data);
+  });
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
@@ -11,4 +34,4 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(port);
+http.listen(port);
